b2cd875e53e13a2aa3a1db7870e0fa1bb2826768
alex <alexj.nich@hotmail.com>
2017-04-27 14:30:38 -0400

    Fixed retry tweet bug
    UpdateError except block had an incorrect
    signature for get_next_update.

--- bot.py
+++ bot.py
@@ -7,15 +7,15 @@ from exceptions import UpdateError
 apis = setup_apis()
 
 next_update = updates.get_next_update(apis)
+last_updated_time = next_update.time
 
 while True:
     try:
         next_update.update()
-        last_updated_time = next_update.time
 
         next_update = updates.get_next_update(apis, last_updated_time)
         sleep(updates.INTERVAL)
 
     except UpdateError as e:
         print(e)
-        next_update = updates.get_next_update()
+        next_update = updates.get_next_update(apis, last_updated_time)
